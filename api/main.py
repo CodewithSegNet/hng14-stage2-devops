03d203a1127c1656e768ab1bf76cd42b2f278551
OlusegunMACS <eeomo007@gmail.com>
2026-04-23 02:56:12 +0100

    :ambulance: add new fix for format issue

--- api/main.py
+++ api/main.py
@@ -6,8 +6,10 @@ import os
 app = FastAPI()
 
 r = redis.Redis(
-    host=os.getenv("REDIS_HOST", "redis"), port=int(os.getenv("REDIS_PORT", 6379))
-)
+    host=os.getenv(
+        "REDIS_HOST", "redis"), port=int(
+            os.getenv(
+                "REDIS_PORT", 6379)))
 
 
 @app.post("/jobs")
--- worker/worker.py
+++ worker/worker.py
@@ -3,7 +3,10 @@ import time
 import os
 
 r = redis.Redis(
-    host=os.getenv("REDIS_HOST", "redis"), port=int(os.getenv("REDIS_PORT", 6379))
+    host=os.getenv("REDIS_HOST", "redis"), 
+    port=int(
+        os.getenv("REDIS_PORT", 6379)
+        )
 )
 
 
